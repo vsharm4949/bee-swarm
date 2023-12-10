5a487111d4560c3177e5fd40c5c64628610ea662
vsharm <vijayasharma4949@gmail.com>
2023-12-10 14:16:49 -0800

    implements Bee interface

--- src/bee.h
+++ src/bee.h
@@ -1,54 +1,116 @@
 #ifndef __BEE_H__
 #define __BEE_H__
 
-struct Coordinate
-{
-    double x;
-    double y;
-};
+#include <random>
+#include <vector>
 
-class Bee {
-public:
-    explicit Bee(
-        Role role,
-        Coordinate position = { 0.0, 0.0 },
-        Coordinate food = { 0.0, 0.0 },
-        double buzz = 5.0
-    ) : role(role), position(position), home(position), buzz(buzz), food(food)
-    {
-    }
-
-    Role get_role() const { return role; }
-
-    void communicate(Role new_role, Coordinate new_food);
-    void scout(double x_move, double y_move);
-    void work(double x_move, double y_move);
-    void go_home();
-
-    bool is_home() const {
-        return (position.y > home.y - buzz)
-            && (position.y < home.y + buzz)
-            && (position.x > home.x - buzz)
-            && (position.x < home.x + buzz)
-    }
-
-    void waggle(double jiggle);
-    Coordinate get_pos() const { return position; }
-    Coordinate get_food() const { return food; }
-
-    void move_home(Coordinate new_home) {
-        home = new_home;
-    }
-
-private:
-    Role role;
-    Coordinate position;
-    Coordinate home;
-    Coordinate food;
-    const double buzz;
-};
-
-
+namespace BeeColony {
+    enum class Role {
+        Worker,
+        Inactive,
+        Scout
+    };
     
+    class Hive;
+
+    struct Coordinate
+    {
+        double x;
+        double y;
+    };
+
+    inline double quality(Coordinate position) {
+        return position.y;
+    }
+
+    Hive make_hive(
+        int number_workers,
+        int number_inactive,
+        int number_scout,
+        Coordinate start_pos,
+        Coordinate food,
+        float bee_size,
+        int steps
+    );
+
+    class Bee {
+    public:
+        explicit Bee(
+            Role role,
+            Coordinate position = { 0.0, 0.0 },
+            Coordinate food = { 0.0, 0.0 },
+            double buzz = 5.0
+        ) : role(role), position(position), home(position), buzz(buzz), food(food)
+        {
+        }
+
+        Role get_role() const { return role; }
+
+        void communicate(Role new_role, Coordinate new_food);
+        void scout(double x_move, double y_move);
+        void work(double x_move, double y_move);
+        void go_home();
+
+        bool is_home() const {
+            return (position.y > home.y - buzz)
+                && (position.y < home.y + buzz)
+                && (position.x > home.x - buzz)
+                && (position.x < home.x + buzz);
+        }
+
+        void waggle(double jiggle);
+        Coordinate get_pos() const { return position; }
+        Coordinate get_food() const { return food; }
+
+        void move_home(Coordinate new_home) {
+            home = new_home;
+        }
+
+    private:
+        Role role;
+        Coordinate position;
+        Coordinate home;
+        Coordinate food;
+        const double buzz;
+    };
+
+    class Hive {
+    public:
+        Hive(
+            int number_workers,
+            int number_inactive,
+            int number_scount,
+            Coordinate start_pos,
+            Coordinate food,
+            float buzz,
+            int steps
+        );
+
+        std::vector<BeeColony::Bee> get_bees() const {
+            return bees;
+        }
+
+        void update_bees();
+        void swarm();
+        bool all_home();
+
+    private:
+        std::vector<Bee> bees;
+        const size_t steps;
+        size_t step;
+        std::mt19937 engine;
+        std::normal_distribution<double> normal_dist;
+        std::uniform_int_distribution<> uniform_dist;
+
+        void waggle_dance();
+        void explore();
+    };
+
+    bool should_swarm(const std::vector<BeeColony::Bee> & bees, double target);
+    void move(Coordinate & from, const Coordinate to, double step);
+}
+
+
+        
 
 #endif
