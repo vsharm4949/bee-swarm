e860e0108f8fcad1b6922319ceeeee57f897a406
vsharm <vijayasharma4949@gmail.com>
2023-12-10 19:37:44 -0800

    completes implementation of Bee and Hive impls

--- src/bee.cpp
+++ src/bee.cpp
@@ -36,4 +36,102 @@ void Bee::waggle(double jiggle) {
     position.x += jiggle;
 }
 
+void Bee::communicate(Role new_role, Coordinate new_food) {
+    role = new_role;
+    if (quality(new_food) > quality(food)) {
+        food = new_food;
+    }
+}
 
+void BeeColony::move(Coordinate& from, const Coordinate to, double step) {
+    if (from.y > to.y) from.y -= step;
+    if (from.y < to.y) from.y += step;
+    if (from.x > to.x) from.x -= step;
+    if (from.x < to.x) from.x += step;
+}
+
+void Bee::work(double x_move, double y_move) {
+    move(position, food, buzz/2.0);
+    double new_quality = quality({ position.x + x_move, position.y + y_move });
+    if (new_quality >= quality(position)) {
+        position.x += x_move;
+        position.y += y_move;
+    }
+}
+
+void Bee::go_home() {
+    if (!is_home()) {
+        move(position, home, buzz);
+    }
+}
+
+void Hive::waggle_dance() {
+    for (auto &bee : bees) {
+        switch (bee.get_role()) {
+            case Role::Worker:
+                bee.work(normal_dist(engine), normal_dist(engine));
+                break;
+            case Role::Scout:
+                bee.scout(normal_dist(engine), normal_dist(engine));
+                break;
+        }
+    }
+}
+
+void Hive::explore() {
+    for (auto &bee : bees) {
+        switch (bee.get_role()) {
+            case Role::Worker:
+                bee.work(normal_dist(engine), normal_dist(engine));
+                break;
+            case Role::Scout:
+                bee.scout(normal_dist(engine), normal_dist(engine));
+                break;
+        }
+    }
+}
+
+void Hive::update_bees() {
+    if (++step < steps) {
+        explore();
+    }
+    else if(!all_home()) {
+        for(auto &bee : bees) {
+            if(!bee.is_home()) bee.go_home();
+            else bee.waggle(normal_dist(engine));
+        }
+    }
+    else {
+        waggle_dance();
+    }
+}
+
+bool Hive::all_home() {
+    return std::all_of(bees.cbegin(), bees.cend(),
+        [](const Bee &individual) { return individual.is_home(); }
+    );
+}
+
+void Hive::swarm() {
+    double best_x = -1.0, best_y = -1.0;
+    for (const auto &bee : bees) {
+        if (quality(bee.get_food()) > best_y) {
+            best_y = bee.get_food().y;
+            best_x = bee.get_food().x;
+        }
+    }
+    for(auto &bee : bees) {
+        bee.move_home({ best_x, best_y });
+    }
+    step = steps;
+}
+
+bool BeeColony::should_swarm(
+    const std::vector<BeeColony::Bee> &bees,
+    double target
+) {
+    return bees.end() == std::find_if(bees.begin(), bees.end(), 
+            [target](const Bee &bee) {
+            return quality(bee.get_food()) < target;
+            });
+}
